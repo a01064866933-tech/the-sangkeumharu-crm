30796dd287a93399fa5c8149b7d20a161a6f05d4
https://github.com/a01064866933-tech <a01064866933@gmail.com>
2026-09-11 19:57:26 +0900

    Add public Supabase client configuration

---
+++ supabase-config.js
@@ -0,0 +1,1 @@
+window.FRESH_DAY_CONFIG={supabaseUrl:'https://bdkgplecpebsuznvbaan.supabase.co',supabaseAnonKey:'sb_publishable_a1J86gLTHEnzRfN8_-5CFQ_tbtcjZOi',kakaoChatUrl:'https://pf.kakao.com/_LlEsG/chat'};
